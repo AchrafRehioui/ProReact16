4ee1278d74db02f9a6b73510dfff1d95e4206bfc
Achraf <achraf.rehioui.1@gmail.com>
2020-12-29 10:57:21 +0100

    first commit

---
+++ primer/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+
+// const message = "This is constant"
+
+// const count = 4;
+
+// function isEven() {
+//   return count % 2 === 0 ? "Even" : "Odd";
+// }
+
+
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 4
+    }
+  }
+  isEven() {
+    return this.state.count % 2 === 0 ? "Even" : "Odd";
+  }
+
+  getClassName(val) {
+    return val % 2 === 0
+      ? "bg-primary text-white text-center p-2 m-1"
+      : "bg-secondary text-white text-center p-2 m-1"
+  }
+
+  handleClick = () => this.setState({ count: this.state.count + 1 });
+
+
+  render = () =>
+    <h4 className={this.getClassName(this.state.count)}>
+      {/* {message} */}
+      <button className="btn btn-info m-2" onClick={this.handleClick}>
+        Click Me
+      </button>
+      Number of things: {this.isEven()}
+    </h4>
+}
+
+
