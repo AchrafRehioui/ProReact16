52b19142abd806d3b325dcacad2775df2351de76
Achraf <achraf.rehioui.1@gmail.com>
2021-01-05 15:44:57 +0100

    Using Function Props

--- components/src/App.js
+++ components/src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { Message } from "./Message";
 import { Summary } from "./Summary";
+import ReactDOM from "react-dom";
 
 let names = ["Bob", "Alice", "Dora"]
 
+function reverseNames() {
+  names.reverse();
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+function promoteName(name) {
+  names = [name, ...names.filter(val => val !== name)];
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
 
 export default function App() {
   return (
-    // <div>
-    //   <h1 className="bg-primary text-white text-center p-2">
-    //     Hello Adam
-    //   </h1>
-    //   <Message greeting="Hello" name="Bob" />
-    //   <Message greeting="Hola" name={"Alice" + "Smith"} />
-    //   <Message greeting="Hi there" name="Dora" />
-    //   {/* <Summary names={["Bob", "Alice", "Dora"]} /> */}
-
-    // </div>
     <table className="table table-sm table-striped">
       <thead>
         <tr><th>#</th><th>Name</th><th>Letters</th></tr>
@@ -24,7 +24,7 @@ export default function App() {
       <tbody>
         {names.map((name, index) =>
           <tr key={name}>
-            <Summary index={index} name={name} />
+            <Summary index={index} name={name} reverseCallback={reverseNames} promoteCallback={promoteName} />
           </tr>
         )}
       </tbody>
--- components/src/Summary.js
+++ components/src/Summary.js
@@ -1,52 +1,22 @@
 import React from "react";
 
 
-// function createInnerElements(names) {
-//     // let arrayElems = [];
-//     // for (let i = 0; i < names.length; i++) {
-//     //     arrayElems.push(
-//     //         <div>
-//     //             {`${names[i]} contains ${names[i].length} letters`}
-//     //         </div>
-//     //     )
-//     // }
-//     // return arrayElems;
-//     return names.map(name =>
-//         <div>
-//             {`${name} contains ${name.length} letters`}
-//         </div>
-//     )
-// }
-
 export function Summary(props) {
 
-    // <h4 className="bg-info text-white text-center p-2">
-    //     {/* { createInnerElements(props.names)} */}
-    //     { props.names.map(name =>
-    //         <div>
-    //             {`${name} contains ${name.length} letters`}
-    //         </div>
-    //     )
-    //     }
-    // </h4>
-    // return (<>
-    //     <td>{props.index + 1} </td>
-    //     <td>{props.name} </td>
-    //     <td>{props.name.length} </td>
-    // </>
-    // )
-
-
-    //attempting changing props  giving error cannot assign to read only property
-    // props.name = `Name: ${props.name}`;
-
-    if (props.name.length >= 4) {
-        return <React.Fragment>
-            <td>{props.index + 1} </td>
-            <td>{props.name} </td>
-            <td>{props.name.length} </td>
-        </React.Fragment>
-    } else {
-        return null;
-    }
+    return (<React.Fragment>
+        <td>{props.index + 1} </td>
+        <td>{props.name} </td>
+        <td>{props.name.length} </td>
+        <td>
+            <button className="btn btn-primary btn-sm"
+                onClick={props.reverseCallback}>
+                Change
+            </button>
+            <button className="btn btn-info btn-sm m-1"
+                onClick={() => props.promoteCallback(props.name)}>
+                Promote
+            </button>
+        </td>
+    </React.Fragment>
+    )
 }
