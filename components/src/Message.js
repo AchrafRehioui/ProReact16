d353935ea3d1e4878f63266fd363d28b9a6340b6
Achraf <achraf.rehioui.1@gmail.com>
2021-01-05 12:24:14 +0100

    Defining Props in Parent Component and Receiving Props in Child Component and combining Javascript and props to render content

--- components/src/Message.js
+++ components/src/Message.js
@@ -1,8 +1,32 @@
 import React from "react";
 
 
-export function Message() {
-    return (<h4 className="bg-success text-white text-center p-2">
-        This is a message
-    </h4>)
+// export function Message(props) {
+
+//     if (props.name === "Bob") {
+
+//         return <h4 className="bg-warning p-2">{props.greeting}, {props.name}</h4>
+//     } else {
+
+//         return <h4 className="bg-success text-white text-center p-2">
+//             {props.greeting}, {props.name}
+//         </h4>
+//     }
+// }
+
+export function Message(props) {
+    let classes;
+    switch (props.name) {
+        case "Bob":
+            classes = "bg-warning p-2";
+            break;
+        case "Dora":
+            classes = "bg-secondary text-white text-center p-2"
+            break;
+        default:
+            classes = "bg-success text-white text-center p-2"
+    }
+    return <h4 className={classes}>
+        {props.greeting}, {props.name}
+    </h4>
 }
--- components/src/Summary.js
+++ components/src/Summary.js
@@ -1,8 +1,52 @@
 import React from "react";
 
-export function Summary() {
-    return (<h4 className="bg-info text-white text-center p-2">
-        This is a summary
-    </h4>
-    )
+
+// function createInnerElements(names) {
+//     // let arrayElems = [];
+//     // for (let i = 0; i < names.length; i++) {
+//     //     arrayElems.push(
+//     //         <div>
+//     //             {`${names[i]} contains ${names[i].length} letters`}
+//     //         </div>
+//     //     )
+//     // }
+//     // return arrayElems;
+//     return names.map(name =>
+//         <div>
+//             {`${name} contains ${name.length} letters`}
+//         </div>
+//     )
+// }
+
+export function Summary(props) {
+
+    // <h4 className="bg-info text-white text-center p-2">
+    //     {/* { createInnerElements(props.names)} */}
+    //     { props.names.map(name =>
+    //         <div>
+    //             {`${name} contains ${name.length} letters`}
+    //         </div>
+    //     )
+    //     }
+    // </h4>
+    // return (<>
+    //     <td>{props.index + 1} </td>
+    //     <td>{props.name} </td>
+    //     <td>{props.name.length} </td>
+    // </>
+    // )
+
+
+    //attempting changing props  giving error cannot assign to read only property
+    // props.name = `Name: ${props.name}`;
+
+    if (props.name.length >= 4) {
+        return <React.Fragment>
+            <td>{props.index + 1} </td>
+            <td>{props.name} </td>
+            <td>{props.name.length} </td>
+        </React.Fragment>
+    } else {
+        return null;
+    }
 }
