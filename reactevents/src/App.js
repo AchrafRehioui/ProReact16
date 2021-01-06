2598611a8df2cf5b2e0e8d3fffea21e0dfe8f789
Achraf <achraf.rehioui.1@gmail.com>
2021-01-06 16:18:23 +0100

    Preventing default Behavior

--- reactevents/src/App.js
+++ reactevents/src/App.js
@@ -19,23 +19,38 @@ export default class App extends Component {
     }, () => this.setState({ message: `${event.type}: ${this.state.counter}` }));
   }
 
+
+  toggleCheckBox = (event) => {
+    if (this.state.counter === 0) {
+      event.preventDefault();
+    }
+  }
+
   render() {
-    return (
-      <div className="m-2">
-        <div className={`h4 bg-${this.state.theme} text-white text-center p-2`}>
-          {this.state.message}
-        </div>
-        <div className="text-center">
-          <button className="btn btn-primary"
-            onClick={(e) => this.handleEvent(e, "primary")} >
-            Normal
-          </button>
-          <button className="btn btn-danger m-1"
-            onClick={(e) => this.handleEvent(e, "danger")} >
-            Danger
-          </button>
-        </div>
+    return <div className="m-2">
+      <div className="form-check">
+        <input className="form-check-input" type="checkbox"
+          onClick={this.toggleCheckBox} />
+        <label>This is a checkbox</label>
       </div>
-    )
+      
+      <div className={`h4 bg-${this.state.theme} text-white text-center p-2`}>
+        {this.state.message}
+      </div>
+
+      <div className="text-center">
+
+        <button className="btn btn-primary"
+          onClick={(e) => this.handleEvent(e, "primary")} >
+          Normal
+        </button>
+
+        <button className="btn btn-danger m-1"
+          onClick={(e) => this.handleEvent(e, "danger")} >
+          Danger
+        </button>
+
+      </div>
+    </div>
   }
 }
