324ed7ef335d1872ad08a03705922d51e5451422
Achraf <achraf.rehioui.1@gmail.com>
2021-01-08 12:15:40 +0100

    using context of global data

--- composition/src/ActionButton.js
+++ composition/src/ActionButton.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { ProModeContext } from "./ProModeContext";
 
 
 export class ActionButton extends Component {
@@ -6,10 +7,21 @@ export class ActionButton extends Component {
 
     render() {
         return (
-            <button className={` btn btn-${this.props.theme} m-2`}
-                onClick={this.props.callback}>
-                { this.props.text}
-            </button>
+            <ProModeContext.Consumer>
+                { contextData =>
+                    <button
+                        className={this.getClasses(contextData.proMode)}
+                        disabled={!contextData.proMode}
+                        onClick={this.props.callback}>
+                        {this.props.text}
+                    </button>
+                }
+            </ProModeContext.Consumer>
         )
     }
-}    
+
+    getClasses(proMode) {
+        let col = proMode ? this.props.theme : "danger";
+        return `btn btn-${col} m-2`;
+    }
+}  
