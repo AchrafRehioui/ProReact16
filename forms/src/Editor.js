f69a83156600a5ad5a2372fd72e0b6bd248903dd
Achraf <achraf.rehioui.1@gmail.com>
2021-01-09 13:10:57 +0100

    validating form data ensuring that a checkbox is selected

--- forms/src/Editor.js
+++ forms/src/Editor.js
@@ -1,36 +1,59 @@
 import React, { Component } from "react";
+import { FormValidator } from "./FormValidator";
+import { ValidationMessage } from "./ValidationMessage";
+
 
 export class Editor extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            name: "Bob",
-            order: ""
+            name: "",
+            terms: false
+        }
+        this.rules = {
+            name: { required: true, minlength: 3, alpha: true },
+            terms: { true: true }
         }
     }
 
     updateFormValue = (event) => {
-        this.setState({ [event.target.name]: event.target.value },
-            () => this.props.submit(this.state));
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    updateFormValueCheck = (event) => {
+        this.setState({ [event.target.name]: event.target.checked });
     }
 
     render() {
         return <div className="h5 bg-info text-white p-2">
-            <div className="form-group">
-                <label>Name</label>
-                <input className="form-control"
-                    name="name"
-                    value={this.state.name}
-                    onChange={this.updateFormValue} />
-            </div>
 
-            <div className="form-group">
-                <label>Order</label>
-                <textarea className="form-control" name="order"
-                    value={this.state.order}
-                    onChange={this.updateFormValue} />
-            </div>
+            <FormValidator data={this.state} rules={this.rules}
+                submit={this.props.submit}>
+
+                <div className="form-group">
+                    <label>Name</label>
+                    <input className="form-control"
+                        name="name"
+                        value={this.state.name}
+                        onChange={this.updateFormValue} />
+                    <ValidationMessage field="name" />
+                </div>
+
+                <div className="form-group">
+                    <div className="form-check">
+                        <input className="form-check-input"
+                            type="checkbox" name="terms"
+                            checked={this.state.terms}
+                            onChange={this.updateFormValueCheck} />
+                        <label className="form-check-label">
+                            Agree to terms
+                        </label>
+                    </div>
+                    <ValidationMessage field="terms" />
+                </div>
+
+            </FormValidator>
         </div>
     }
 }
