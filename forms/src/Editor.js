057e9d75b55b435192ca0ccebef3beb1cb1b3994
Achraf <achraf.rehioui.1@gmail.com>
2021-01-09 12:26:54 +0100

    using text area

--- forms/src/Editor.js
+++ forms/src/Editor.js
@@ -6,22 +6,12 @@ export class Editor extends Component {
         super(props);
         this.state = {
             name: "Bob",
-            flavor: "Vanilla",
-            twoScoops: false
+            order: ""
         }
-        this.flavors = ["Chocolate", "Double Chocolate",
-            "Triple Chocolate", "Vanilla"];
-        this.toppings = ["Sprinkles", "Fudge Sauce",
-            "Strawberries", "Maple Syrup"]
     }
 
     updateFormValue = (event) => {
         this.setState({ [event.target.name]: event.target.value },
-            () => this.props.submit(this.state));
-    }
-
-    updateFormValueCheck = (event) => {
-        this.setState({ [event.target.name]: event.target.checked },
             () => this.props.submit(this.state));
     }
 
@@ -34,29 +24,12 @@ export class Editor extends Component {
                     value={this.state.name}
                     onChange={this.updateFormValue} />
             </div>
+
             <div className="form-group">
-                <label>Ice Cream Flavors</label>
-                {this.flavors.map(flavor =>
-                    <div className="form-check" key={flavor}>
-                        <input className="form-check-input"
-                            type="radio" name="flavor"
-                            value={flavor}
-                            checked={this.state.flavor === flavor}
-                            onChange={this.updateFormValue} />
-                        <label className="form-check-label">
-                            {flavor}
-                        </label>
-                    </div>
-                )}
-            </div>
-            <div className="form-group">
-                <div className="form-check">
-                    <input className="form-check-input"
-                        type="checkbox" name="twoScoops"
-                        checked={this.state.twoScoops}
-                        onChange={this.updateFormValueCheck} />
-                    <label className="form-check-label">Two Scoops</label>
-                </div>
+                <label>Order</label>
+                <textarea className="form-control" name="order"
+                    value={this.state.order}
+                    onChange={this.updateFormValue} />
             </div>
         </div>
     }
