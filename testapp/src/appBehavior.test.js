253730e36251dde994fdc3e9959a4657f820b6e1
Achraf <achraf.rehioui.1@gmail.com>
2021-01-10 01:06:12 +0100

    testing the effects of an event

--- testapp/src/appBehavior.test.js
+++ testapp/src/appBehavior.test.js
@@ -20,11 +20,32 @@ it("uses title prop", () => {
 
 
 it("updates state data", () => {
+
     const wrapper = shallow(<App />);
+
     const values = [10, 20, 30];
+
     values.forEach((val, index) =>
         wrapper.instance().updateFieldValue(index + 1, val));
+
     wrapper.instance().updateTotal();
+
     expect(wrapper.state("total"))
         .toBe(values.reduce((total, val) => total + val), 0);
 });
+
+it("updates total when button is clicked", () => {
+    const wrapper = shallow(<App />);
+
+    const button = wrapper.find("button").first();
+
+    const values = [10, 20, 30];
+
+    values.forEach((val, index) =>
+        wrapper.instance().updateFieldValue(index + 1, val));
+
+    button.simulate("click")
+
+    expect(wrapper.state("total"))
+        .toBe(values.reduce((total, val) => total + val), 0);
+})
