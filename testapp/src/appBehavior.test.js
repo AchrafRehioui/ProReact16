d7179e8abc87e5e29c27b4ee07d632d5530b250b
Achraf <achraf.rehioui.1@gmail.com>
2021-01-10 01:02:29 +0100

    testing the effect of Methods

---
+++ testapp/src/appBehavior.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow } from "enzyme";
+import App from "./App";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+it("uses title prop", () => {
+
+    const titleVal = "test title"
+    const wrapper = shallow(<App title={titleVal} />);
+
+    const firstTitle = wrapper.find("h5").text();
+    const stateValue = wrapper.state("title");
+
+    expect(firstTitle).toBe(titleVal);
+    expect(stateValue).toBe(titleVal);
+
+});
+
+
+it("updates state data", () => {
+    const wrapper = shallow(<App />);
+    const values = [10, 20, 30];
+    values.forEach((val, index) =>
+        wrapper.instance().updateFieldValue(index + 1, val));
+    wrapper.instance().updateTotal();
+    expect(wrapper.state("total"))
+        .toBe(values.reduce((total, val) => total + val), 0);
+});
