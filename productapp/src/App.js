f7db39a3d42e50b0c033821750fd00d41697b968
Achraf <achraf.rehioui.1@gmail.com>
2021-01-11 15:00:14 +0100

    using redux data store API - creating connector component

--- productapp/src/App.js
+++ productapp/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 //import ProductsAndSuppliers from "./ProductsAndSuppliers";
 import { Provider } from "react-redux";
-import dataStore from "./store";
+import dataStore, { deleteProduct } from "./store";
 import { Selector } from "./Selector";
 import { ProductDisplay } from "./ProductDisplay";
 import { SupplierDisplay } from "./SupplierDisplay";
 import { StoreAccess } from "./store/StoreAccess";
+import { CustomConnector, CustomConnectorProvider } from "./store/CustomConnector";
+import { startEditingProduct } from "./store/stateActions";
+import { ProductTable } from "./ProductTable";
+
+
+const selectors = {
+  products: (store) => store.modelData.products
+}
+
+const dispatchers = {
+  editCallback: startEditingProduct,
+  deleteCallback: deleteProduct
+}
 
 
 export default class App extends Component {
@@ -23,6 +36,17 @@ export default class App extends Component {
               <SupplierDisplay name="Suppliers" />
             </Selector>
           </Provider>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col">
+          <CustomConnectorProvider dataStore={dataStore}>
+            <CustomConnector selectors={selectors}
+              dispatchers={dispatchers}>
+              <ProductTable />
+            </CustomConnector>
+          </CustomConnectorProvider>
         </div>
       </div>
     </div>
