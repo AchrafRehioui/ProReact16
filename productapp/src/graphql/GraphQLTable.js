f14a4ff8d91b70d6315e45e320e483db46fa4f75
Achraf <achraf.rehioui.1@gmail.com>
2021-01-12 17:39:45 +0100

    using a Graphql client framework - creating a graphql component

---
+++ productapp/src/graphql/GraphQLTable.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import * as queries from "./queries";
+import { ProductTable } from "../ProductTable";
+
+export const GraphQLTable = () => {
+
+    const getAll = gql(queries.products.getAll.graphql);
+
+    return class extends Component {
+
+        constructor(props) {
+            super(props);
+            this.editCallback = (item) => this.props.history
+                .push(`/products/edit/${item.id}`);
+        }
+
+        render() {
+            return <Query query={getAll}>
+                {({ loading, data, refetch }) => {
+                    if (loading) {
+                        return <h5
+                            className="bg-info text-white text-center m-2 p-2">
+                            Loading...
+                            </h5>
+                    } else {
+                        return <React.Fragment>
+                            <ProductTable products={data.products}
+                                editCallback={this.editCallback}
+                                deleteCallback={() => { }} />
+                            <div className="text-center">
+                                <button className="btn btn-primary"
+                                    onClick={() => refetch()}>
+                                    Reload Data
+                                </button>
+                            </div>
+                        </React.Fragment>
+                    }
+                }}
+            </Query>
+        }
+    }
+}
