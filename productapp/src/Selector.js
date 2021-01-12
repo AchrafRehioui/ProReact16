5f6c8d193dcd4c4314ce43a47eb9e266b0d645ba
Achraf <achraf.rehioui.1@gmail.com>
2021-01-12 11:00:48 +0100

    consuming Web Service - creating data source component

--- productapp/src/Selector.js
+++ productapp/src/Selector.js
@@ -2,53 +2,32 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect }
     from "react-router-dom";
 import { ToggleLink } from "./routing/ToggleLink";
-//import { CustomPrompt } from "./routing/CustomPrompt";
 import { RoutedDisplay } from "./routing/RoutedDisplay";
-
-
-// const RouteInfoHOC = withRouter(RouteInfo)
+import { IsolatedTable } from "./IsolatedTable";
 
 
 export class Selector extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            showPrompt: false,
-            message: "",
-            callback: () => { }
-        }
-    }
-
-    customGetUserConfirmation = (message, navCallback) => {
-        this.setState({
-            showPrompt: true, message: message,
-            callback: (allow) => {
-                navCallback(allow);
-                this.setState({ showPrompt: false })
-            }
-        });
-    }
 
     render() {
-
         const routes = React.Children.map(this.props.children, child => ({
             component: child,
             name: child.props.name,
             url: `/${child.props.name.toLowerCase()}`,
             datatype: child.props.datatype
         }));
-
         return <Router getUserConfirmation={this.customGetUserConfirmation}>
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-2">
+                        <ToggleLink to="/isolated">Isolated Data</ToggleLink>
                         {routes.map(r => <ToggleLink key={r.url} to={r.url}>
                             {r.name}
                         </ToggleLink>)}
                     </div>
                     <div className="col">
                         <Switch>
+                            <Route path="/isolated" component={IsolatedTable} />
                             {routes.map(r =>
                                 <Route key={r.url}
                                     path={`/:datatype(${r.datatype})/:mode?/:id?`}
