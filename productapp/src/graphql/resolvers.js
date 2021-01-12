9736d8dced18dbda7bac30ea615b75ba3823d3b4
Achraf <achraf.rehioui.1@gmail.com>
2021-01-12 12:47:40 +0100

    making graphql queries - creating queries with arguments

--- productapp/src/graphql/resolvers.js
+++ productapp/src/graphql/resolvers.js
@@ -2,5 +2,24 @@ var data = require("../../restData")();
 
 module.exports = {
     products: () => data.products,
-    suppliers: () => data.suppliers
+
+    product: ({ id }) => data.products.find(p => p.id === parseInt(id)),
+
+    suppliers: () => data.suppliers.map(s => ({
+        ...s, products: () => s.products.map(id =>
+            data.products.find(p => p.id === Number(id)))
+    })),
+
+
+    supplier: ({ id }) => {
+        const result = data.suppliers.find(s => s.id === parseInt(id));
+
+        if (result) {
+            return {
+                ...result,
+                products: () => result.products.map(id =>
+                    data.products.find(p => p.id === Number(id)))
+            }
+        }
+    }
 }
