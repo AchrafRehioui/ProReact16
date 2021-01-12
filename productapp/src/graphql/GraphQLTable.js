117f169976063ff4323bbafb97ace353ba175b3f
Achraf <achraf.rehioui.1@gmail.com>
2021-01-12 17:45:14 +0100

    using Graphql client framework - using mutations

--- productapp/src/graphql/GraphQLTable.js
+++ productapp/src/graphql/GraphQLTable.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-import { Query } from "react-apollo";
+import { Query, Mutation } from "react-apollo";
 import gql from "graphql-tag";
 import * as queries from "./queries";
 import { ProductTable } from "../ProductTable";
+import * as mutations from "./mutations";
 
 export const GraphQLTable = () => {
 
     const getAll = gql(queries.products.getAll.graphql);
+    const deleteItem = gql(mutations.products.delete.graphql);
+
 
     return class extends Component {
 
@@ -25,17 +28,23 @@ export const GraphQLTable = () => {
                             Loading...
                             </h5>
                     } else {
-                        return <React.Fragment>
-                            <ProductTable products={data.products}
-                                editCallback={this.editCallback}
-                                deleteCallback={() => { }} />
-                            <div className="text-center">
-                                <button className="btn btn-primary"
-                                    onClick={() => refetch()}>
-                                    Reload Data
-                                </button>
-                            </div>
-                        </React.Fragment>
+                        return <Mutation mutation={deleteItem}
+                            refetchQueries={() => [{ query: getAll }]}>
+                            {doDelete =>
+                                <React.Fragment>
+                                    <ProductTable products={data.products}
+                                        editCallback={this.editCallback}
+                                        deleteCallback={(p) =>
+                                            doDelete({ variables: { id: p.id } })} />
+                                    <div className="text-center">
+                                        <button className="btn btn-primary"
+                                            onClick={() => refetch()}>
+                                            Reload Data
+                                        </button>
+                                    </div>
+                                </React.Fragment>
+                            }
+                        </Mutation>
                     }
                 }}
             </Query>
